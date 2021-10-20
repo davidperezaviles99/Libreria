4a83b86686ab8313b2a1966557ec3fefafe98e91
davidperezaviles99 <45419830+davidperezaviles99@users.noreply.github.com>
2021-10-20 18:03:05 +0200

    Patron Singleton añadido

--- libreria/src/main/java/com/libreria/repository/Persona.java
+++ libreria/src/main/java/com/libreria/repository/Persona.java
@@ -2,12 +2,24 @@ package com.libreria.repository;
 
 public class Persona {
 	
-	private String nombre = "David";
-
+	private String nombre;
+	
+	private static Persona unico;
+	
 	public Persona(String nombre) {
 		this.nombre = nombre;
+		System.out.println("Mi nombre es: " +this.nombre);
 	}
-
+	
+	public static Persona getSingletonInstance(String nombre) {
+		if (unico == null) {
+			unico = new Persona(nombre);
+		} else {
+			System.out.println("No se puede crear el objeto " +nombre);
+		}
+		return unico;
+	}
+	
 	public String getNombre() {
 		return nombre;
 	}
